ed8864a86c246f5176307c14517b7051867d8e68
JHS1790 <1790@holbertonschool.com>
2020-09-15 20:09:49 +0000

    reading from buffer from file, no functionality yet

--- main_monty.c
+++ main_monty.c
@@ -1,4 +1,4 @@
-#include "queen.h"
+#include "monty.h"
 
 /**
  * main - an interpreter for Monty ByteCodes files
@@ -8,5 +8,23 @@
  */
 int main(int argc, char **argv)
 {
+	char buffer[1024];
+	FILE *file;
 
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit (EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	/* File is open*/
+
+	fgets(buffer, 1024, file);
+	printf("buffer:%s", buffer);
+
+	fclose(file);
+	/* File is closed*/
+
+	exit (EXIT_SUCCESS);
 }
