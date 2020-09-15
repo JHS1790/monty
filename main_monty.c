05d4d63a439de8749c5f5648e04d6fab4341cdc7
JHS1790 <1790@holbertonschool.com>
2020-09-15 16:34:13 +0000

    created main file

---
+++ main_monty.c
@@ -0,0 +1,12 @@
+#include "queen.h"
+
+/**
+ * main - an interpreter for Monty ByteCodes files
+ * @argv: arguments vector - the arguments as an array of strings passed in
+ * @argc: arguments count - how many arguments are passed in
+ * Return: 0 on success, XX error codes
+ */
+int main(int argc, char **argv)
+{
+
+}
