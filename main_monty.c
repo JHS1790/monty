86c0218d0ed5d5538285046b1671d2d1106bde3a
GreenCyberNinja <1824@holbertonschool.com>
2020-09-16 05:25:16 +0000

    implemented ops and loop to check if in dictionary\betty compliant

--- main_monty.c
+++ main_monty.c
@@ -1,6 +1,5 @@
 #include "monty.h"
 int global_token_int;
-
 /**
  * main - an interpreter for Monty ByteCodes files
  * @argv: arguments vector - the arguments as an array of strings passed in
@@ -17,7 +16,7 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 
 	file = fopen(argv[1], "r");
@@ -39,26 +38,43 @@ int main(int argc, char **argv)
 	fclose(file);
 	/* File is closed*/
 
-	exit (EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
 
 /**
- *
- *
- * BETTY
- *
- *
+ * check_opcode - checks the token if its an opcode
+ * @stack: the stack to be passed if opcode
+ * @line_number: the line number for error
+ * @token_opcode: the opcode hopefully
+ * @token_int: to be turned into a global variable
+ * Return: 1 on success or 0 on failure
  */
-int check_opcode(stack_t *stack, unsigned int line_number, char *token_opcode, char *token_int)
+int check_opcode(stack_t *stack, unsigned int line_number,
+char *token_opcode, char *token_int)
 {
+	int i;
+	instruction_t ops[] = {
+			{"push",/* *blah*/},
+			{"pall", /*bleh*/},
+			{"pint", /*blih*/},
+			{"pop", /*bloh*/},
+			{"swap", /*bluh*/},
+			{"add", /*blyh8*/},
+			{"nop", /*ran out of nouns*/},
+			{NULL, /*NULL*/} };
+
 	if (token_int)
 		global_token_int = atoi(token_int);
-
-	printf("global token = %d\n", global_token_int);
-
 	(void)stack;
 	(void)line_number;
-	(void)token_opcode;
+	for (i = 0; ops[i].opcode != NULL; i++)
+	{
+		if (!strcmp(token_opcode, ops[i].opcode))
+		{
+			printf("success fuckface\n");
+			return (1);
+		}
+	}
 
 	return (0);
 }
